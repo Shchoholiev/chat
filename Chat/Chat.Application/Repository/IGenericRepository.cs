1f9369358b42ca123a6feefe6db511e7dfd7f50e
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-04-16 15:28:01 +0200

    Added Repositories Interfaces and classes for Paging.

---
+++ Chat/Chat.Application/Repository/IGenericRepository.cs
@@ -0,0 +1,29 @@
+﻿using Chat.Application.Paging;
+using System.Linq.Expressions;
+
+namespace Chat.Application.Repository
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task AddAsync(TEntity item);
+
+        Task UpdateAsync(TEntity item);
+
+        Task DeleteAsync(TEntity item);
+
+        void Attach(params object[] obj);
+
+        Task<TEntity> GetOneAsync(int id);
+
+        Task<TEntity> GetOneAsync(int id, params Expression<Func<TEntity, object>>[] includeProperties);
+
+        Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters);
+
+        Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters,
+                                              params Expression<Func<TEntity, object>>[] includeProperties);
+
+        Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters, 
+                                              Expression<Func<TEntity, bool>> predicate,
+                                              params Expression<Func<TEntity, object>>[] includeProperties);
+    }
+}

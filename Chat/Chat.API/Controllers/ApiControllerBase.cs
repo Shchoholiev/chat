14e9cfcfd05a74d07950697ac7572f5ee63d2c9e
DESKTOP-J5SGACQ\rareb <sh.codework@gmail.com>
2022-08-21 10:02:05 +0300

    Added ApiControllerBase

---
+++ Chat/Chat.API/Controllers/ApiControllerBase.cs
@@ -0,0 +1,27 @@
+﻿using Chat.Application.Paging;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Claims;
+
+namespace Chat.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApiControllerBase : ControllerBase
+    {
+        protected string? Email => User?.Claims?.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+
+        protected void SetPagingMetadata(IPagedList pagedList)
+        {
+            var metadata = new
+            {
+                pagedList.PageSize,
+                pagedList.PageNumber,
+                pagedList.TotalPages,
+                pagedList.HasNextPage,
+                pagedList.HasPreviousPage
+            };
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metadata));
+        }
+    }
+}
